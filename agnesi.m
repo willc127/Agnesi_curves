216dcba371d56a5cd7e0d9a0fc92697017c45f64
William Cesar <williamcesar.ribeiro@outlook.com>
2023-01-15 16:04:18 -0300

    Improve graphics

--- agnesi.m
+++ agnesi.m
@@ -1,11 +1,18 @@
-function y = agnesi(x,a,rgb)
+function y= agnesi(x,a,rgb)
   %Define Agnesi function
   y = 8*a.^3./(x.^2+4*a.^2);
- plot(x,y, 'color', rgb)
+  txt = ['a = ', num2str(a)];
+  h = plot(x, y, 'linewidth', 2.5, 'color', rgb,'DisplayName',txt);
+  xlabel('x')
+  ylabel('y')
+  set(get(gca,'ylabel'),'rotation',0)
+  legend('location', 'eastoutside', 'fontsize', 12);
+  %Define a circle with radius = a
   hold on
   x0 = 0;
   y0 = a;
   r = a;
-  h = drawCircle(x0,y0,r, 'color', rgb)
-axis([min(x) max(x) 0 max(y)]);
-daspect ([1 1])
+  hc = drawCircle(x0, y0, r, 'color', rgb, 'HandleVisibility','off');
+  %plot configurations
+  axis([-40 40 0 max(y)*1.1]);
+  daspect ([1 1]);
--- main.m
+++ main.m
@@ -1,13 +1,14 @@
 %pkg install "https://downloads.sourceforge.net/project/octave/Octave%20Forge%20Packages/Individual%20Package%20Releases/matgeom-1.2.3.tar.gz"
 %pkg load matgeom
-close all
+close all; clear all; clc
 %Define the vector of a
-a = [1:7]
+a = [1:7];
 %Define the colormap
-colmap =jet(length(a));
+colmap = jet(length(a));
+%Open new figure
 fig = figure(1)
 for i = 1:length(a)
-  x = linspace(-12*a(i),12*a(i),100);
-  agnesi(x,a(i),colmap(i,:));
+  x = linspace(-12*a(i),12*a(i),250);
+  agnesi(x, a(i), colmap(i,:));
 end
-print (fig, "Agnesi_curves.png");
+print (fig, "Agnesi_curves.png","-S1000,500");
